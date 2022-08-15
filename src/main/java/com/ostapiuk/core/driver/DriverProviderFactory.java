33a7f6b2eb326204d2048ca7015804911d285a90
Volodymyr Ostapiuk <volodymy.ostapiuk@cartera.com>
2022-08-15 11:16:16 +0300

    Install chromedriver in docker

--- src/main/java/com/ostapiuk/core/driver/DriverProviderFactory.java
+++ src/main/java/com/ostapiuk/core/driver/DriverProviderFactory.java
@@ -1,6 +1,5 @@
 package com.ostapiuk.core.driver;
 
-import com.ostapiuk.core.properties.ConfigProperties;
 import io.github.bonigarcia.wdm.DriverManagerType;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.PageLoadStrategy;
@@ -9,16 +8,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.io.File;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Objects;
-
 public enum DriverProviderFactory {
     CHROME {
         @Override
         public WebDriver create() {
-            new ChromeDriverLoader().loadChromeDriver();
+//            new ChromeDriverLoader().loadChromeDriver();
+            WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
             ChromeOptions options = new ChromeOptions();
             options.setPageLoadStrategy(PageLoadStrategy.EAGER);
             return new ChromeDriver(options);
